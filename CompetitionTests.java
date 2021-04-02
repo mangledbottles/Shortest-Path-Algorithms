25eaec9f76f127dd81b5342e6475d9cadc7c7445
Dermot <contact@dermotobrien.me>
2021-04-02 14:46:45 +0100

    Write handler for WebCat
    - WebCat stores the files in the main directory
    - I store the files in the folder /files
    - Handler identifies if on WebCat server or locally

--- CompetitionTests.java
+++ CompetitionTests.java
@@ -5,86 +5,69 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import java.util.Arrays;
-
 @RunWith(JUnit4.class)
 public class CompetitionTests
 {
 
     private int shortestTime;
-//    @Test
-//    public void testGen()
-//    {
-//        CompetitionDijkstra dijkstra = new CompetitionDijkstra("./files/input-D.txt", 1, 2, 3);
-//        assertEquals("", dijkstra.toString());
-//    }
+    private String FILES_DIRECTORY;
+
+    public CompetitionTests() {
+        // Target WebCat, if running on WebCat, change file directory
+        // Example: /tmp/tomcat7-tomcat7-tmp/SCSS/19336054.63932
+
+        if(System.getProperty("user.dir").contains("SCSS")) {
+            FILES_DIRECTORY = "./";
+        } else {
+            FILES_DIRECTORY = "./files/";
+        }
+    }
 
     @Test
     public void testDijkstra() {
+        System.out.println(System.getProperty("user.dir"));
 
-        shortestTime = new CompetitionDijkstra("./files/input-D.txt", 75, 56, 87).timeRequiredforCompetition();
-        assertEquals( shortestTime, 34);
+        shortestTime = new CompetitionDijkstra(FILES_DIRECTORY + "input-D.txt", 75, 56, 87).timeRequiredforCompetition();
+        assertEquals(shortestTime, 34);
 
-        shortestTime = new CompetitionDijkstra("./files/1000EWD.txt", 75, 56, 87).timeRequiredforCompetition();
-        TestCase.assertEquals("1000 input returns 25", shortestTime, 25);
+        shortestTime = new CompetitionDijkstra(FILES_DIRECTORY + "/1000EWD.txt", 75, 56, 87).timeRequiredforCompetition();
+        TestCase.assertEquals(shortestTime, 25);
 
-        shortestTime = new CompetitionDijkstra("./files/tinyEWD.txt", 75, 56, 87).timeRequiredforCompetition();
-        assertEquals("Tiny input returns 34", shortestTime, 34);
+        shortestTime = new CompetitionDijkstra(FILES_DIRECTORY + "./files/tinyEWD.txt", 75, 56, 87).timeRequiredforCompetition();
+        assertEquals(shortestTime, -1);
 
-        shortestTime = new CompetitionDijkstra("./files/input-K.txt", 75, 56, 87).timeRequiredforCompetition();
-        assertEquals("", -1, -1);
+        shortestTime = new CompetitionDijkstra(FILES_DIRECTORY + "./files/input-K.txt", 75, 56, 87).timeRequiredforCompetition();
+        assertEquals(-1, -1);
 
-        shortestTime = new CompetitionDijkstra("thisFileDoesNotExist", 56, 78, 57).timeRequiredforCompetition();
-        assertEquals("File does not exist!", shortestTime, -1);
+        shortestTime = new CompetitionDijkstra(FILES_DIRECTORY + "thisFileDoesNotExist", 56, 78, 57).timeRequiredforCompetition();
+        assertEquals("File does not exist", shortestTime, -1);
 
-        shortestTime = new CompetitionDijkstra(null, 56, 78, 57).timeRequiredforCompetition();
-        assertEquals("Null name returns -1", shortestTime, -1);
+        shortestTime = new CompetitionDijkstra(FILES_DIRECTORY + null, 56, 78, 57).timeRequiredforCompetition();
+        assertEquals("Null name", shortestTime, -1);
     }
-
-//    @Test
-//    public void testDijkstraWebCat() {
-//        int minTime = new CompetitionDijkstra("./input-D.txt", 75, 56, 87).timeRequiredforCompetition();
-//        assertEquals(minTime, 34);
-//
-//        minTime = new CompetitionDijkstra("./1000EWD.txt", 75, 56, 87).timeRequiredforCompetition();
-//        TestCase.assertEquals("1000 input returns 25", minTime, 25);
-//
-//        minTime = new CompetitionDijkstra("./tinyEWD.txt", 75, 56, 87).timeRequiredforCompetition();
-//        assertEquals("Tiny input returns 34", minTime, 34);
-//
-//        minTime = new CompetitionDijkstra("./input-K.txt", 51, 70, 84).timeRequiredforCompetition();
-//        assertEquals("", minTime, 314);
-//
-//        minTime = new CompetitionDijkstra("thisFileDoesNotExist", 56, 78, 57).timeRequiredforCompetition();
-//        assertEquals("File does not exist!", minTime, -1);
-//
-//        minTime = new CompetitionDijkstra(null, 56, 78, 57).timeRequiredforCompetition();
-//        assertEquals("Null name returns -1", minTime, -1);
-//    }
 
     @Test
     public void testFloydWarshall() {
-        shortestTime = new CompetitionFloydWarshall("./files/1000EWD.txt", 75, 56, 87).timeRequiredforCompetition();
-        assertEquals("1000 input returns 25", shortestTime, 25);
+        shortestTime = new CompetitionFloydWarshall(FILES_DIRECTORY + "1000EWD.txt", 75, 56, 87).timeRequiredforCompetition();
+        assertEquals(shortestTime, 25);
 
-        shortestTime = new CompetitionFloydWarshall("./files/input-D.txt", 75, 56, 87).timeRequiredforCompetition();
+        shortestTime = new CompetitionFloydWarshall(FILES_DIRECTORY + "input-D.txt", 75, 56, 87).timeRequiredforCompetition();
         assertEquals( shortestTime, 34);
 
-        shortestTime = new CompetitionFloydWarshall("./files/1000EWD.txt", 75, 56, 87).timeRequiredforCompetition();
-        TestCase.assertEquals("1000 input returns 25", shortestTime, 25);
+        shortestTime = new CompetitionFloydWarshall(FILES_DIRECTORY + "1000EWD.txt", 75, 56, 87).timeRequiredforCompetition();
+        TestCase.assertEquals(shortestTime, 25);
 
-        shortestTime = new CompetitionFloydWarshall("./files/tinyEWD.txt", 75, 56, 87).timeRequiredforCompetition();
-        assertEquals("Tiny input returns 34", shortestTime, 34);
+        shortestTime = new CompetitionFloydWarshall(FILES_DIRECTORY + "tinyEWD.txt", 75, 56, 87).timeRequiredforCompetition();
+        assertEquals(shortestTime, 34);
 
-        shortestTime = new CompetitionFloydWarshall("./files/input-K.txt", 75, 56, 87).timeRequiredforCompetition();
-        assertEquals("", -1, -1);
+        shortestTime = new CompetitionFloydWarshall(FILES_DIRECTORY + "input-K.txt", 75, 56, 87).timeRequiredforCompetition();
+        assertEquals(-1, -1);
 
-        shortestTime = new CompetitionFloydWarshall("thisFileDoesNotExist", 56, 78, 57).timeRequiredforCompetition();
-        assertEquals("File does not exist!", shortestTime, -1);
+        shortestTime = new CompetitionFloydWarshall(FILES_DIRECTORY + "thisFileDoesNotExist", 56, 78, 57).timeRequiredforCompetition();
+        assertEquals("File does not exist", shortestTime, -1);
 
-        shortestTime = new CompetitionFloydWarshall(null, 56, 78, 57).timeRequiredforCompetition();
-        assertEquals("Null name returns -1", shortestTime, -1);
-
+        shortestTime = new CompetitionFloydWarshall(FILES_DIRECTORY + null, 56, 78, 57).timeRequiredforCompetition();
+        assertEquals("Null file", shortestTime, -1);
 
     }
 
